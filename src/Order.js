60d4db426f54fab23d8053b39ff742c230e08e4d
BinitShrestha08 <binitshrestha24@gmail.com>
2023-06-12 18:02:33 +1000

    Something

--- src/Order.js
+++ src/Order.js
@@ -1,8 +1,9 @@
 export default function Order({ closeHour }) {
   return (
     <div className="order">
-      <p>We're open until {closeHour}:00. Come visit us or order online</p>
-      <button className="btn">Order</button>
+      <button className="btn" onClick={() => alert("Order Successful !")}>
+        Order
+      </button>
     </div>
   );
 }
