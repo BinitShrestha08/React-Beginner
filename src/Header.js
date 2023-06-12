3c2b7267a393a3fc2c0f8ab3ab5bb11cbb958efd
BinitShrestha08 <binitshrestha24@gmail.com>
2023-06-12 14:12:13 +1000

    check

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
   return (
     <header className="header">
-      <h1>Fast React Pizza Co.</h1>
+      <h1>Fast React-Beginner Pizza Co.</h1>
     </header>
   );
 }
